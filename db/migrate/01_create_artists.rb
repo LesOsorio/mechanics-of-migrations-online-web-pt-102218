9dc2361a64507ac432edfdce1fb5196a2a696c38
Leslie Osorio <lesosorio17@gmail.com>
2019-03-19 21:27:53 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -10,16 +10,4 @@ class CreateArtists < ActiveRecord::Migration
     end
   end
 
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS artists (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    genre TEXT,
-    age INTEGER,
-    hometown TEXT
-    )
-  SQL
-
-ActiveRecord::Base.connection.execute(sql)
-
 end
